eaf3973f4d78fd7b4d53dced39fb1fea5f2e1d25
rifqi.aditya <aditya.rifqi30@gmail.com>
2023-05-24 18:43:37 +0700

    Add Url Launcher

--- lib/ui/widgets/home_tips_item.dart
+++ lib/ui/widgets/home_tips_item.dart
@@ -1,10 +1,12 @@
 import 'package:bank_sha/shared/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomeTipsItem extends StatelessWidget {
   final String imageUrl;
   final String title;
   final String url;
+
   const HomeTipsItem(
       {required this.imageUrl,
       required this.title,
@@ -13,38 +15,46 @@ class HomeTipsItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 170,
-      height: 176,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: whiteColor,
-      ),
-      child: Column(
-        children: [
-          ClipRRect(
-            borderRadius: const BorderRadius.vertical(top: Radius.circular(20)),
-            child: Image.asset(
-              imageUrl,
-              width: 170,
-              height: 110,
-              fit: BoxFit.cover,
-            ),
-          ),
-          const SizedBox(height: 12),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 12),
-            child: Text(
-              title,
-              style: blackTextStyle.copyWith(
-                fontSize: 12,
-                fontWeight: medium,
-                overflow: TextOverflow.ellipsis,
+    return GestureDetector(
+      onTap: () async {
+        if (await canLaunchUrl(Uri.parse(url))) {
+          launchUrl(Uri.parse(url));
+        }
+      },
+      child: Container(
+        width: 170,
+        height: 176,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: whiteColor,
+        ),
+        child: Column(
+          children: [
+            ClipRRect(
+              borderRadius:
+                  const BorderRadius.vertical(top: Radius.circular(20)),
+              child: Image.asset(
+                imageUrl,
+                width: 170,
+                height: 110,
+                fit: BoxFit.cover,
               ),
-              maxLines: 2,
             ),
-          ),
-        ],
+            const SizedBox(height: 12),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: Text(
+                title,
+                style: blackTextStyle.copyWith(
+                  fontSize: 12,
+                  fontWeight: medium,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                maxLines: 2,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
